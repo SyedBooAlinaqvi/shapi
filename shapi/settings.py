435fec2868465807b1b0dc0bd6e9adc3029ff7bc
SyedBooAlinaqvi <syed.booali2@gmail.com>
2021-06-19 18:44:30 +0500

    thirdcommitruntime

--- shapi/settings.py
+++ shapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,3 +141,5 @@ CLOUDINARY_STORAGE = {
         'API_KEY': '394429588748526',
         'API_SECRET': 'f_JNR4OZDo0XG6UHsJlmKRSAY7E'
 }
+
+django_heroku.settings(locals())
